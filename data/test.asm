56c976978ec0a55391a9a0954030c22684e5aabc
Ekta Khante <khanteekta@gmail.com>
2024-12-06 14:11:37 +0530

    Update test.asm

--- data/test.asm
+++ data/test.asm
@@ -1,2 +1,16 @@
-MOV AX,BX
-MOV BX,AX
+; This is a simple program with data directives and instructions
+
+START:
+    MOV AX, 5              ; Move value 5 into AX register
+    ADD AX, 10             ; Add 10 to AX
+    MOV BX, 20             ; Move value 20 into BX register
+    JMP END                ; Jump to label END
+
+DATA_SEG:
+    DB 0x01, 0x02, 0x03    ; Define byte values
+    DW 0x1234, 0x5678      ; Define word values
+    RESB 5                 ; Reserve 5 bytes
+    RESW 2                 ; Reserve 2 words
+
+END:
+    HLT                    ; Halt the program
